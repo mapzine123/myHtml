49db2637bbebd43ba0cc0be2d1c288589b6179ee
un <66024439+mapzine123@users.noreply.github.com>
2022-04-01 16:01:33 +0900

    많이쓰는 string 상수로 설저

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -1,19 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let tabcontent = document.getElementsByClassName("tabcontent");
+    const TABCONTENT = "tabcontent";
+    const ACTIVE = "active";
+    const HIDDEN = "hidden";
+    let tabcontent = document.getElementsByClassName(TABCONTENT);
     let tablinks = document.querySelectorAll(".tablinks");
     const cityNames = ["London", "Paris", "Tokyo"];
     function openCity() {
         let i;
-        const flag = this.classList.contains("active");
+        const flag = this.classList.contains(ACTIVE);
         for(i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].classList.add("hidden");
+            tabcontent[i].classList.add(HIDDEN);
         }
         for(i = 0; i < tablinks.length; i++) {
-            tablinks[i].classList.remove("active");
+            tablinks[i].classList.remove(ACTIVE);
         }
         if(!flag) {
-            document.getElementById(this.value).classList.toggle("hidden");
-            this.classList.add("active");
+            document.getElementById(this.value).classList.toggle(HIDDEN);
+            this.classList.add(ACTIVE);
         }
     }
     tablinks.forEach(element => element.addEventListener("click", openCity));
