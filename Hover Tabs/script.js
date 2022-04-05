53b2f35b2f7ab0f32f83ab0234f6daa915897eea
un <66024439+mapzine123@users.noreply.github.com>
2022-04-05 16:13:25 +0900

    HoverTab

--- Hover Tabs/script.js
+++ Hover Tabs/script.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
+    let tablinks = document.querySelectorAll(".tablinks");
+    let tabContent = document.querySelectorAll(".tabcontent");
+    hidden(tabContent);
+
+    function hidden(elements) {
+        elements.forEach(element => element.classList.add("hidden"));
+    }
+
+    function openCity(event) {
+        let currentPage = event.target.innerText;
+        let currentContent = document.querySelector("#" + currentPage);
+        hidden(tabContent);
+        currentContent.classList.remove("hidden");
+    }
+    tablinks.forEach(element => element.addEventListener("mouseover", openCity));
+
 
 });
